111988725b4d6c073d17f240777bdd41e045f783
shahar <shahar@hyperguest.com>
2022-10-09 13:52:29 +0300

    fix(deploy): module_variable_optional_attrs is not supported

--- infrastructures/modules/Component/providers.tf
+++ infrastructures/modules/Component/providers.tf
@@ -1,6 +1,5 @@
 terraform {
 
-  experiments = [module_variable_optional_attrs]
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -12,3 +11,4 @@ terraform {
     }
   }
 }
+
