d424878e0a566f1ab8e41ecb0feacc4c1ae5be2f
shahar <shahar@hyperguest.com>
2022-10-10 18:39:43 +0300

    feat(build): issue #2

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -5,10 +5,17 @@ export KDT_WORKDIR=$(pwd)
 function clone_if_not_exist() {
     KASPA_REPOS_DIR="$KDT_WORKDIR/kaspa-repos"
     COMPONENT_EXPECTED_DIR="$KASPA_REPOS_DIR/$1"
-    echo $COMPONENT_EXPECTED_DIR
     if [ ! -d $COMPONENT_EXPECTED_DIR ]; then
         cd KASPA_REPOS_DIR
         sudo -u $USER git clone $2 $COMPONENT_EXPECTED_DIR
+    fi
+}
+
+function create_symlk_if_not_exits() {
+    KASPA_REPOS_DIR="$KDT_WORKDIR/kaspa-repos"
+    COMPONENT_EXPECTED_DIR="$KASPA_REPOS_DIR/$1"
+    if [ ! -d $COMPONENT_EXPECTED_DIR ]; then
+        ln -s $2 $COMPONENT_EXPECTED_DIR
     fi
 }
 
@@ -18,9 +25,12 @@ for row in $(cat $CONFIG_PATH | jq -r '.[] | @base64'); do
     _jq() {
         echo ${row} | base64 --decode | jq -r ${1}
     }
-    if []
-    clone_if_not_exist $(_jq '.name') $(_jq '.git')
+    if [ ! "$(_jq '.path')" = "null" ]; then
+        create_symlk_if_not_exits $(_jq '.name') "$(_jq '.path')"
+    else
+        clone_if_not_exist $(_jq '.name') $(_jq '.git')
+    fi
 done
 
 cd "$KDT_WORKDIR/infrastructures"
-cdktf get
+# cdktf get
